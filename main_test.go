d94dba6720a7069e24122a2f798913321bedeb51
浅雨痕 <inkedawn@gmail.com>
2018-03-31 23:29:41 +0800

    测试用例

--- main_test.go
+++ main_test.go
@@ -3,10 +3,11 @@ package main
 import (
 	"testing"
 	"time"
+	"./utility"
 )
 
 func TestTime(t *testing.T)  {
 	e := time.Now()
-	b := e.Add(time.Duration(randRange(-35, -25))*time.Minute)
+	b := e.Add(time.Duration(utility.RandRange(-35, -25))*time.Minute)
 	t.Logf("%v\n%v", b.Format(timePattern), e.Format(timePattern))
 }
