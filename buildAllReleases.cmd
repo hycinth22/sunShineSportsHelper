1980a1d5685d078041bd8bb6df71e695bddd4eed
风痕 <inkedawn@gmail.com>
2019-04-30 17:46:29 +0800

    UPDATE buildAllReleases.cmd

--- buildAllReleases.cmd
+++ buildAllReleases.cmd
@@ -4,12 +4,12 @@ rem win64
 SET CGO_ENABLED=0
 SET GOOS=windows
 SET GOARCH=amd64
-go build -o releases/sunShineSportsHelper-win-x64.exe
+go build -o releases/sunShineSportsHelper-win-amd64.exe
 
 rem linux 64
 SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=amd64
-go build -o releases/sunShineSportsHelper-linux-x64
+go build -o releases/sunShineSportsHelper-linux-amd64
 
-xcopy /-Y /R build_assets build releases
+xcopy /-Y /R build_assets assets\releases\
